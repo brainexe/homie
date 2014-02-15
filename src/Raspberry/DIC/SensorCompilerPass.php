c06522543f97b3b069c4ad2c034ed2aea33e4d1c
Matze <matze@mdoetsch.de>
2014-02-15 21:38:17 +0100

    added forgotten annotation

--- src/Raspberry/DIC/SensorCompilerPass.php
+++ src/Raspberry/DIC/SensorCompilerPass.php
@@ -7,6 +7,9 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
+/**
+ * @DI\Service(tags={{"name" = "compiler_pass"}})
+ */
 class SensorCompilerPass implements CompilerPassInterface {
 
 	public function process(ContainerBuilder $container) {
