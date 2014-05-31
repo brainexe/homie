149b44c28d6e3067245208353e5ce1859ad90914
Matze <matze@mdoetsch.de>
2014-05-31 17:03:50 +0200

    add todo reminder

---
+++ src/Raspberry/TodoList/Command/SendTodoReminderCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Raspberry\TodoList\Command;
+
+use Matze\Core\Console\AbstractCommand;
+use Matze\Core\Traits\ServiceContainerTrait;
+use Raspberry\TodoList\TodoReminder;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @Command
+ */
+class SendTodoReminderCommand extends AbstractCommand {
+
+	use ServiceContainerTrait;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function configure() {
+		$this
+			->setName('todo:reminder')
+			->setDescription('Send removder mail');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doExecute(InputInterface $input, OutputInterface $output) {
+		/** @var TodoReminder $todo_reminder */
+		$todo_reminder = $this->getService('TodoReminder');
+		$todo_reminder->sendNotification();
+	}
+} 
---
+++ src/Raspberry/TodoList/TodoReminder.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Raspberry\TodoList;
+
+use Matze\Core\Traits\EventDispatcherTrait;
+use Raspberry\Espeak\EspeakEvent;
+use Raspberry\Espeak\EspeakVO;
+
+/**
+ * @Service
+ */
+class TodoReminder {
+
+	use EventDispatcherTrait;
+
+	/**
+	 * @var TodoList
+	 */
+	private $_todo_list;
+
+	/**
+	 * @Inject({"@TodoList"})
+	 */
+	public function __construct(TodoList $todo_list) {
+		$this->_todo_list = $todo_list;
+	}
+
+	public function sendNotification() {
+		$todos = $this->_todo_list->getList();
+
+		$issues_per_state = [];
+		foreach ($todos as $todo) {
+			if (TodoItemVO::STATUS_COMPLETED === $todo->status)	{
+				continue;
+			}
+
+			$issues_per_state[$todo->status][] = $todo;
+		}
+
+		if (empty($issues_per_state)) {
+			return;
+		}
+
+		$text = _('Erinnerung');
+		$text .= ': ';
+
+		foreach ($issues_per_state as $state => $issues_per_status) {
+			$text .= $this->_getStateName(count($issues_per_status), $state);
+			$text .= ': ';
+			foreach ($issues_per_status as $todo) {
+				$text .= $todo->name.". ";
+			}
+		}
+
+		$espeak_vo = new EspeakVO($text);
+		$event = new EspeakEvent($espeak_vo);
+
+		$this->dispatchInBackground($event);
+	}
+
+	/**
+	 * @param integer $count
+	 * @param string $state
+	 * @return string
+	 */
+	private function _getStateName($count, $state) {
+		switch ($state) {
+			case TodoItemVO::STATUS_PROGRESS:
+				return sprintf(ngettext('%d offene Aufgabe', '%d offene Aufgaben', $count), $count);
+			case TodoItemVO::STATUS_PENDING:
+				return sprintf(ngettext('%d Aufgabe in Arbeit', '%d offene Aufgaben in Arbeit', $count), $count);
+			default:
+				return '';
+		}
+	}
+
+
+}
