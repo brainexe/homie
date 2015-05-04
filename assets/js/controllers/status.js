9a35c392da50e5d9b8f4eb092d2d460d6b8877d3
matze <matze@mdoetsch.de>
2015-05-04 21:57:53 +0200

    removed SelfUpdate

--- assets/js/controllers/status.js
+++ assets/js/controllers/status.js
@@ -1,5 +1,4 @@
 App.ng.controller('StatusController', ['$scope', function($scope) {
-	$scope.textarea = '';
 	$scope.stats    = {};
 	$scope.jobs     = {};
 
@@ -16,14 +15,6 @@ App.ng.controller('StatusController', ['$scope', function($scope) {
     setInterval(function() {
         $scope.update();
     }, 1000);
-
-	$scope.startUpdate = function() {
-		if (!confirm('Do you want to start self update? This may take some time...')) {
-			return false;
-		}
-
-		$.post('/stats/self_update/');
-	};
 
 	$scope.resetStats = function(key) {
 		var url = '/stats/reset/'.format(key);
@@ -42,12 +33,4 @@ App.ng.controller('StatusController', ['$scope', function($scope) {
 			$scope.$apply();
 		});
 	};
-
-	App.Layout.$scope.$on('update.process', function(event) {
-		$scope.textarea += event.payload;
-	});
-
-	App.Layout.$scope.$on('update.done', function(event) {
-		$scope.textarea += "Update is done!";
-	});
 }]);
