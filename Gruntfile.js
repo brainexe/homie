173841da037e3503c84921a8f98f3f930f4cb66b
matze <matze@mdoetsch.de>
2015-05-12 22:49:59 +0200

    added "grunt watch" task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,31 @@
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-angular-gettext');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('extract_lang', ['nggettext_extract']);
     grunt.registerTask('compile_lang', ['nggettext_compile']);
+
+    grunt.registerTask('console', function(task) {
+        var child = grunt.util.spawn({
+            cmd: 'php',
+            args: ['console', task],
+            stdio: 'inherit'
+        });
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+
+        console.log('php console ' +  task);
+    });
 
     grunt.initConfig({
         nggettext_extract: {
             pot: {
                 files: {
                     'lang/template.pot': [
-                        'assets/templates/*.html',
-                        'assets/templates/*/*.html',
-                        'assets/js/*.js',
-                        'assets/js/*/*.js',
-                        'assets/js/*/*/*.js',
-                        'templates/*.html'
+                        'assets/templates/**/*.html',
+                        'assets/js/**/*.js',
+                        'templates/**/*.html'
                     ]
                 }
             }
@@ -25,6 +35,16 @@ module.exports = function(grunt) {
                 files: {
                     'assets/lang/translations.js': ['lang/*.po']
                 }
+            }
+        },
+        watch: {
+            assets: {
+                files: ['assets/**'],
+                tasks: ['console:cc']
+            },
+            src: {
+                files: ['src/**'],
+                tasks: ['console:cc']
             }
         }
     });
